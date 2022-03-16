878ddbf588f44141c737276b79e174bc5c21b514
Levente26 <madi.levente26@gmail.com>
2022-03-16 17:44:41 +0100

    base repo with db config

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Helio auth</title>
+</head>
+<body class="container grey lighten-2">
+    
+</body>
+</html>
