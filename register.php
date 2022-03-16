91681fd88f29857be292c979d6e2771c759da0cf
Levente26 <madi.levente26@gmail.com>
2022-03-16 18:03:25 +0100

    registration completed

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php 
     include('config/db.php');
-    $username = $email = $password = "";
+    $username = $email = $password = $passwordagain = "";
     $errors = array('email' => '', 'username' => '', 'password' => '');
 
     if(isset($_POST['submit'])){
@@ -22,8 +22,10 @@
         }
 
         // check password
-        if(empty($_POST['password'])){
-            $errors['ingredients'] = 'Password is required <br />';
+        if(empty($_POST['password']) || empty($_POST['passwordagain'])){
+            $errors['password'] = 'Password is required <br />';
+        } else if($_POST['password'] != $_POST['passwordagain']) {
+            $errors['password'] = 'Passwords do not match <br />';
         } else {
             $password = $_POST['password'];
         }
@@ -58,12 +60,17 @@
             <label for="">Add Your Email</label>
             <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
             <div class="red-text"><?php echo $errors['email'] ?></div>
+
             <label for="">Add Your Username</label>
             <input type="text" name="username" value="<?php echo htmlspecialchars($username) ?>">
             <div class="red-text"><?php echo $errors['username'] ?></div>
+
             <label for="">Add Your Password</label>
             <input type="password" name="password" value="<?php echo htmlspecialchars($password) ?>">
+            <label for="">Add Your Password Again</label>
+            <input type="password" name="passwordagain" value="<?php echo htmlspecialchars($passwordagain) ?>">
             <div class="red-text"><?php echo $errors['password'] ?></div>
+            
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
             </div>
